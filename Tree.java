ba3e9a30fda50d4bfd14047c6e88522907157770
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2019-11-14 05:22:48 -0500

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,3 +1,16 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.*;
+import java.applet.*;
+
 class Tree{
-// This draws my lollipop shaped tree 
+// This draws my lollipop shaped tree
+public static void drawTrunk(Graphics g){
+//This draws my floors
+}
+
+public static void drawLeaves(Graphics g){
+//This draws my roof
+}
+
 }
