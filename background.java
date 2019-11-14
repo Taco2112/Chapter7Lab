e0192daa74c051a8f5be784043657f8f909395a5
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2019-11-14 05:23:40 -0500

    Update background.java

--- background.java
+++ background.java
@@ -1,11 +1,24 @@
-class Background{
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.*;
+import java.applet.*;
+
+class background{
 // This is my background
   public static void drawSky(Graphics g){
 //This draws the blue sky
+  g.drawRect(0, 0, 640, 400);
+  g.setColor(Color.blue);
+  g.fillRect(0, 0, 640, 400);
+
 }
 
 public static void drawGrass(Graphics g){
-//This draws the green grass 
+//This draws the green grass
+//g.setColor(Color.green);
+  g.drawRect(0, 200, 640, 400);
+  g.setColor(Color.green);
+  g.fillRect(0, 200, 640, 400);
 }
 
 
