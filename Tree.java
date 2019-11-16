d6b2f0e514d7de8a59ba79c30141dede4281c8b2
TACO2112 <45360167+Taco2112@users.noreply.github.com>
2019-11-15 20:36:02 -0500

    Update Tree.java
    something nice

--- Tree.java
+++ Tree.java
@@ -6,11 +6,20 @@ import java.applet.*;
 class Tree{
 // This draws my lollipop shaped tree
 public static void drawTrunk(Graphics g){
-//This draws my floors
+//This draws my tree trunk(AKA that box that holds the leaves up... idk(sigh))
+g.setColor(new Color(148, 100, 15));
+g.drawRect(500, 100, 30, 200);
+g.setColor(new Color(148, 100, 15));
+g.fillRect(500, 100, 30, 200);
 }
 
 public static void drawLeaves(Graphics g){
-//This draws my roof
+//This draws my leaves for the Tree
+g.setColor(new Color(0, 255,1));
+g.drawOval(465, 50, 100, 100 );
+g.setColor(new Color(0, 255,1));
+g.fillOval(465, 50, 100, 100);
+
 }
 
 }
